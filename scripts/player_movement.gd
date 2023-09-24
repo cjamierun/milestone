25af93589f9ace88d88423d4792d2747ae725726
Jamie Crane <cjamierun@gmx.com>
2023-09-24 19:10:52 +0300

    TWEAK player movement values

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 7.5
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
